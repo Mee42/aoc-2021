66b5424be89a24fb17b2cfe8e4ca7f5ff836fbd2
Carson Graham <carson42g@gmail.com>
2021-12-15 10:27:26 -0500

    works but doesn't terminate

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -13,10 +13,25 @@ val test = """
 1293138521
 2311944581
 """.trimIndent()
+
+fun Int.wrap() = if(this > 9) this - 9 else this
+
 fun main() {
     val inputRaw = if(1 == 0) test else input(15, 2021)
-    val input = Array2D.from(inputRaw.trim().lines().map { it.map(::id).map { x -> "$x".toInt() } }.transpose())
-    input.print()
+    val inputSmall: List<List<Int>> = inputRaw.trim().lines().map { it.map(::id).map { x -> "$x".toInt() } }
+
+    val incrAmount = Array2D.from((0..4).map { x -> (0..4).map { y -> x + y } })
+    incrAmount.print()
+    // extend each row 5 times
+    val inputBigRows: List<List<Int>> =
+        inputSmall.map { xRow -> (0..4).flatMap { off -> xRow.map { (it + off) } }  } // map each row
+    // extend the entire thing 5 times
+    val inputBigCols: List<List<Int>> =
+        (0..4).flatMap { off -> inputBigRows.map { list -> list.map { (it + off).wrap() } } }
+    val input = Array2D.from(inputBigCols.transpose())
+    input.print(fixedSpace = 0)
+    //return
+//
     val unvisited = input.coordsList.toSet().toMutableSet()
     val visited = mutableSetOf<Coords2D>()
     val probableDistance= mutableMapOf<Coords2D, Int>(point(0, 0) to 0)
@@ -24,6 +39,7 @@ fun main() {
     val goal = point(input.xSize() - 1, input.ySize() - 1)
     val path = mutableListOf<Coords2D>()
     val prev = mutableMapOf<Coords2D, Coords2D>()
+
     while(true) {
         if(current == goal) {
             println("found goal")
@@ -38,17 +54,17 @@ fun main() {
             }
            break
         }
-        println("current: $current")
+        println("current: $current visited: ${visited.size} / ${input.xSize() * input.ySize()}")
         val neighbors = CARDINAL_OFFSETS.map { it + current }
-        println("neigbors: $neighbors")
+        //println("neigbors: $neighbors")
         for(n in neighbors.filter { input.isInBound(it) }) {
             if(n in visited) continue
-            println("looking at neighbor $n")
+            //println("looking at neighbor $n")
             val dist = probableDistance[current]!! + input[n]
             val currentDist = probableDistance[n] ?: Int.MAX_VALUE
-            println("dist $dist currentDist $currentDist")
+            //println("dist $dist currentDist $currentDist")
             if(dist < currentDist) {
-                println("set")
+                //println("set")
                 probableDistance[n] = dist
                 prev[n] = current
             }
